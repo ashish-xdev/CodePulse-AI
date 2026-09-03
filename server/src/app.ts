1cae8f990d63d99790ca8fa67628934aeab2560c
Ashish Tiwari <ashishtiwari8382@gmail.com>
2026-09-03 15:44:29 +0530

    feat: integrate codefile and analysis routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
-import { errorMiddleware } from "./errors/error.middleware.js";
 import cookieParser from "cookie-parser";
 
+import { errorMiddleware } from "./errors/error.middleware.js";
 import userRoutes from "./routes/User.routes.js";
+import codeFileRoutes from "./routes/CodeFile.routes.js";
+import analysisRoutes from "./routes/Analysis.routes.js";
 
 const app = express();
 
@@ -12,8 +14,10 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-// Routes will come here
+// Routes
 app.use("/api/users", userRoutes);
+app.use("/api/codefiles", codeFileRoutes);
+app.use("/api/analyses", analysisRoutes);
 
 // Global error handler — must be last
 app.use(errorMiddleware);
